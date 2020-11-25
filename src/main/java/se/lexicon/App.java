cf85c8646d2effc3e1b84f2909355feca6e6c0ce
Simon Elbrink <Simon.Elbrink@gmail.com>
2020-11-25 12:44:37 +0100

    After todays lecture

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,15 +1,16 @@
 package se.lexicon;
 
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
-        A object = new A();
+/*        A object = new A();
         object.foo();
 
         B objectB = new B();
         objectB.foo();
-
+*/
+        Lion lionName = new Lion();
+        lionName.makeASound();
 
     }
 }
@@ -33,5 +34,85 @@ class B extends A{
     }
 }
 
-// Sub-class
+// Sub-class of B
 class C extends B{}
+
+//Dont actually need to extends Object, it's already there.
+class Mammal extends Object{}
+
+
+class Animal extends Mammal{
+
+    private String name;
+    private double weight;
+
+    //Constructors
+    public Animal(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public Animal() {
+    }
+
+    //Behavior
+    public void makeASound(){
+        System.out.println("This animal makes no Sound");
+    }
+
+    protected Animal foo(){
+        return null;
+    }
+
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
+
+//"CATS"
+class Feline extends Animal{
+
+}
+
+class Tiger extends Feline{}
+
+class Lion extends Feline{
+
+    public void makeASound(){
+        System.out.println("This is a Lion it Roars!");
+    }
+
+    @Override
+    public Lion foo(){
+        return null;
+    }
+
+}
+
+
+//"DOGS"
+
+class Canine extends Animal{
+    private int legs;
+    private int tail;
+    private int teeth;
+}
+
+class Dog extends Canine{}
+
+class Wolf extends Canine{}
+
+
